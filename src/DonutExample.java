4be8ca0801ebd41aaab1878a014d271604da7e3c
Marshall Munoz <Marshall.Munoz@fbitn.com>
2017-11-14 10:47:33 -0600

    donut example creadeted

---
+++ src/DonutExample.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class DonutExample extends JFrame {
+
+    public DonutExample(){
+
+        initUI();
+    }
+
+    private void initUI(){
+
+        add(new Board());
+
+        setSize(330,330);
+
+        setTitle("Donut");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args){
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                DonutExample ex = new DonutExample();
+                ex.setVisible(true);
+            }
+        });
+    }
+}
